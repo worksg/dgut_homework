fe6e6b0b4f66cec112dc26c0ec62e89ed9d4d274
worksg <571940753@qq.com>
2018-06-08 11:18:40 +0800

    socket experiment

---
+++ computer_network/Web_Server/webserver.py
@@ -0,0 +1,46 @@
+# import socket module
+import socket
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Prepare a server socket
+serverSocket.bind(('0.0.0.0', 6789))  # 将TCP欢迎套接字绑定到指定端口
+serverSocket.listen(10)  # 最大连接数为1
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()  # 接收到客户连接请求后，建立新的TCP连接套接字
+    try:
+        message = connectionSocket.recv(1024)  # 获取客户发送的报文
+        message_split = message.split()
+        if len(message_split) == 0:
+            continue
+        print(message_split)
+        filename = message.split()[1]
+        print(filename)
+        f = open(filename[1:])
+        outputdata = f.read()
+        # Send one HTTP header line into socket
+        header = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nContent-Length: %d\n\n' % (
+            len(outputdata))
+        connectionSocket.send(header.encode())
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        header = 'HTTP/1.1 404 NOT FOUND\nConnection: close\nContent-Type: text/html\n'
+        connectionSocket.send(header.encode())
+
+        # Close client socket
+        connectionSocket.close()
+
+serverSocket.close()
+
+
+"""
+curl --noproxy 192.168.14.1 -i -X POST --data "hello_world.html" 192.168.14.1:6789
+"""
+
