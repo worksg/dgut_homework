3eb87b8cca6961bacaf8a71b84b2a7b7f040bb5c
worksg <571940753@qq.com>
2018-06-16 20:45:51 +0800

    fix bug

--- computer_network/Web_Server/webserver.py
+++ computer_network/Web_Server/webserver.py
@@ -6,41 +6,41 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('0.0.0.0', 6789))  # 将TCP欢迎套接字绑定到指定端口
 serverSocket.listen(10)  # 最大连接数为10
 
-while True:
-    # Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()  # 接收到客户连接请求后，建立新的TCP连接套接字
-    try:
-        message = connectionSocket.recv(1024)  # 获取客户发送的报文
-        message_split = message.split()
-        if len(message_split) == 0:
-            continue
-        print(message_split)
-        filename = message.split()[1]
-        print(filename)
-        f = open(filename[1:])
-        outputdata = f.read()
-        # Send one HTTP header line into socket
-        header = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nContent-Length: %d\n\n' % (
-            len(outputdata))
-        connectionSocket.send(header.encode())
-
-        # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.close()
-    except IOError:
-        # Send response message for file not found
-        header = 'HTTP/1.1 404 NOT FOUND\nConnection: close\n'
+try:
+    while True:
+        # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()  # 接收到客户连接请求后，建立新的TCP连接套接字
         try:
+            message = connectionSocket.recv(1024)  # 获取客户发送的报文
+            message_split = message.split()
+            if len(message_split) == 0:
+                continue
+            print(message_split)
+            filename = message.split()[1]
+            print(filename)
+            outputdata = open(filename[1:], encoding="utf-8").read()
+            # Send one HTTP header line into socket
+            header = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nContent-Length: %d\n\n' % (
+                len(outputdata))
             connectionSocket.send(header.encode())
+
+            # Send the content of the requested file to the client
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+
+        except IOError:
+            # Send response message for file not found
+            header = 'HTTP/1.1 404 NOT FOUND\nConnection: close\n'
+            try:
+                connectionSocket.send(header.encode())
+            except Exception:
+                pass
+            # Close client socket
+            connectionSocket.close()
         except Exception:
             pass
-        # Close client socket
-        connectionSocket.close()
-    except Exception:
-        pass
-
-serverSocket.close()
-
-
+        finally:
+            connectionSocket.close()
+finally:
+    serverSocket.close()
